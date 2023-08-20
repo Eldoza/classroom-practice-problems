f0819687fd10a3fbedd114a12aba018015dfb506
Chris Mendoza <cmend85@gmail.com>
2023-08-20 12:26:20 -0500

    Added min and max test

--- MinAndMax/index.js
+++ MinAndMax/index.js
@@ -9,7 +9,37 @@
 // Rules:
 // No there will never be the 2 different elements for the same amount of times of min or max
 // Example [1,1,2,2,3,3,3] => Does NOT need to be accounted for. 
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+}
 
 exports.minAndMax = (itemArray) => {
-  console.log({ itemArray });
+  //Set up object to keep track of names and number of times things appear
+  let countMap = {}
+
+  // Loop over array and add name of element and increase the count by one if it has already been seen
+  for (const num of itemArray){
+    countMap[num] = countMap[num] ? countMap[num] + 1 : 1
+  }
+
+  // Isolate all of the vaues from the countMap so we can figure out what is the min and max number of times they appeared
+  let allValues = Object.values(countMap)
+
+  //Use the max library to get the min and max values
+  let minValue = Math.min(...allValues)
+  let maxValue = Math.max(...allValues)
+
+  // Use a helper function we created to plug that value back into the map to get the key
+  let minCountName = getKeyByValue(countMap, minValue)
+  let maxCountName = getKeyByValue(countMap, maxValue)
+
+// use all of the name and value information to build an object that we need to return to fulfill our requirements and pass all tests
+  return {
+    min: {
+       [minCountName]: minValue
+      },
+    max: {
+      [maxCountName]: maxValue
+    }
+  }
 };
--- MinAndMax/minAndMax.test.js
+++ MinAndMax/minAndMax.test.js
@@ -1,23 +1,23 @@
 const { minAndMax } = require("./index");
 
 test("should return min: { 3: 1 } AND max: { 1: 2 }", () => {
-  expect(minAndMax([1, 1, 3])).toBe({ min: { 3: 1 }, max: { 1: 2 } });
+  expect(minAndMax([1, 1, 3])).toStrictEqual({ min: { 3: 1 }, max: { 1: 2 } });
 });
 
 test("should return min: { 3: 1 } AND max: { 5: 3 }", () => {
-  expect(minAndMax([1, 1, 3, 2, 4, 4, 5, 5, 5, 9, 9, 2])).toBe({
+  expect(minAndMax([1, 1, 3, 2, 4, 4, 5, 5, 5, 9, 9, 2])).toStrictEqual({
     min: { 3: 1 },
     max: { 5: 3 },
   });
 });
 
 test("should return min: { 17: 2 } AND max: { 7: 4 }", () => {
-  expect(minAndMax([17, 4, 5, 5, 3, 3, 3, 4, 5, 7, 7, 7, 7, 4, 17])).toBe({
+  expect(minAndMax([17, 4, 5, 5, 3, 3, 3, 4, 5, 7, 7, 7, 7, 4, 17])).toStrictEqual({
     min: { 17: 2 },
     max: { 7: 4 },
   });
 });
 
 test("should return min: { b: 1 } AND max: { a: 2 }", () => {
-  expect(minAndMax(["a", "a", "b"])).toBe({ min: { b: 1 }, max: { a: 2 } });
+  expect(minAndMax(["a", "a", "b"])).toStrictEqual({ min: { b: 1 }, max: { a: 2 } });
 });
